71278ab9677eac83e680ca4ab1741f5a47202254
root <7356241a>
2019-08-14 18:34:28 +0800

    修复问题

--- core-boot-starters/web-core-boot-starter/src/main/java/com/xzn/core/handler/GlobalExceptionHandler.java
+++ core-boot-starters/web-core-boot-starter/src/main/java/com/xzn/core/handler/GlobalExceptionHandler.java
@@ -12,15 +12,11 @@ import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.servlet.http.HttpServletResponse;
 
-@ControllerAdvice(basePackages = "com.xzn")
-@ResponseBody
 public class GlobalExceptionHandler {
   private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
